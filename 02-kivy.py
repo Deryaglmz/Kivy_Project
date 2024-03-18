c105f5fb0810920178bb45f67c53ce3a614d8ad1
Derya Gülmez <deryaglmz0@gmail.com>
2024-03-18 11:02:33 +0300

    Login screen app

---
+++ 02-kivy.py
@@ -0,0 +1,29 @@
+from kivy.app import App
+from kivy.uix.label import Label
+from kivy.uix.textinput import TextInput
+from kivy.uix.gridlayout import GridLayout
+
+class giris(GridLayout):
+  
+  def __init__(self, **kwargs):
+    super(giris,self).__init__(**kwargs)
+    self.cols = 2
+
+    self.kullaniciadi = Label(text = "User Name: ")
+    self.add_widget(self.kullaniciadi)
+    self.kullanici = TextInput(multiline = False)
+    self.add_widget(self.kullanici)
+
+    self.sifreyazi = Label(text = "Password: ")
+    self.add_widget(self.sifreyazi)
+    self.sifre = TextInput(multiline = False)
+    self.add_widget(self.sifre)
+
+
+class Uygulama(App):
+
+ def build(self):
+    return giris()
+ 
+if __name__ == "__main__":
+    Uygulama().run()
